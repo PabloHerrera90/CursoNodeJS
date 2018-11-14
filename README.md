118b26d65b66375a493b03c2f5bdd04e866745ff
PabloHerrera <pablohb_3169@hotmail.com>
2018-11-13 20:57:53 -0600

    rest del recurso list
    uso de GET y POSt

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Proyecto de prueba
+
+GET: obtener informacion - parametros
+POST: para guardar informacion - payload/parametros
+PUT: actualizar informacion - payload/parametros
+DELETE: eliminar informacion - parametros
--- Server.js
+++ Server.js
@@ -1,6 +1,22 @@
-const http = require('http');
+const Hapi = require('hapi');
+const Ruta = require('./Rutas');
 
-http.createServer(function(req,res){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end('Hola mundo');
-}).listen(3000);
+const server = Hapi.Server({
+    host: 'localhost',
+    port: 3000
+});
+
+
+server.route(Ruta);
+
+async function start(){
+    try{
+        await server.start();
+    } catch (error){
+        console.log(error);
+        process.exit(1);
+    }
+    console.log('Server running at:', server.info.uri);
+}
+
+start();
